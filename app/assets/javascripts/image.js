b015f22ff1432cb254a2af64e0bdd070947c6897
fscarpin <fernando.scarpin@gmail.com>
2016-07-12 19:34:43 -0300

    Moved the jquery from the partial image form to it's own file (image.js)

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,18 +1,20 @@
-$(document).ready(
-  function(){
-    $('#image-btn-upload').attr('disabled',true);
-    $('#image-cancel-btn').attr('disabled', true)
+var enableDisableImageUploadButtons = function() {
+  $('#image-btn-upload').attr('disabled',true);
+  $('#image-cancel-btn').attr('disabled', true)
 
-    $('input:file').change(
-      function(){
-          if ($(this).val()){
-              $('#image-btn-upload').removeAttr('disabled');
-              $('#image-cancel-btn').removeAttr('disabled');
-          }
-          else {
-              $('#image-btn-upload').attr('disabled',true);
-              $('#image-cancel-btn').attr('disabled',true);
-          }
-      });
-  });
+  $('input:file').change(
+    function() {
+      if ($(this).val()) {
+        $('#image-btn-upload').removeAttr('disabled');
+        $('#image-cancel-btn').removeAttr('disabled');
+      }
+      else {
+        $('#image-btn-upload').attr('disabled',true);
+        $('#image-cancel-btn').attr('disabled',true);
+      }
+    });
+}
 
+$(document).ready(function() {
+  enableDisableImageUploadButtons();
+});
